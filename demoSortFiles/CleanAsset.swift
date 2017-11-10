821fd0c42ffe42fa28ee80a6249899f40aef7ff4
Hao <lehuongchu@gmail.com>
2017-11-10 08:04:33 +0700

    commit 8:04 10/11

---
+++ demoSortFiles/CleanAsset.swift
@@ -0,0 +1,9 @@
+//
+//  CleanAsset.swift
+//  demoSortFiles
+//
+//  Created by Hao on 11/10/17.
+//  Copyright © 2017 Hao. All rights reserved.
+//
+
+import Foundation
---
+++ demoSortFiles/sortFileTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  sortFileTableViewCell.swift
+//  demoSortFiles
+//
+//  Created by Hao on 11/5/17.
+//  Copyright © 2017 Hao. All rights reserved.
+//
+
+import UIKit
+
+class sortFileTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var infoSizeOfTypeLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+
+}
